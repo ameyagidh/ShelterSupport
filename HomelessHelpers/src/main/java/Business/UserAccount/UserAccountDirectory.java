bfd9ba4c2f53a70dd20f991a06161485973fe12e
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-11-01 01:18:17 -0400

    Update UserAccountDirectory.java

--- HomelessHelpers/src/main/java/Business/UserAccount/UserAccountDirectory.java
+++ HomelessHelpers/src/main/java/Business/UserAccount/UserAccountDirectory.java
@@ -8,10 +8,7 @@ import Business.Employee.Employee;
 import Business.Role.Role;
 import java.util.ArrayList;
 
-/**
- *
- * @author amey
- */
+
 public class UserAccountDirectory {
 
     private ArrayList<UserAccount> userAccountList;
