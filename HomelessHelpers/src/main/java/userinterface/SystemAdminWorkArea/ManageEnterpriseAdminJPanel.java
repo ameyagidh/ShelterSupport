69671613104914628b38b4ae023993d4f66c9144
Ameya Santosh Gidh <65457905+ameyagidh@users.noreply.github.com>
2023-11-01 01:24:11 -0400

    Update ManageEnterpriseAdminJPanel.java

--- HomelessHelpers/src/main/java/userinterface/SystemAdminWorkArea/ManageEnterpriseAdminJPanel.java
+++ HomelessHelpers/src/main/java/userinterface/SystemAdminWorkArea/ManageEnterpriseAdminJPanel.java
@@ -16,10 +16,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author rinki
- */
+
 public class ManageEnterpriseAdminJPanel extends javax.swing.JPanel {
 
     private JPanel userProcessContainer;
